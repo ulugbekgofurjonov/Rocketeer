6685ce0da40787b7c76f50db9bc526474cfeffcb
Ulug'bek G'ofurjonov <ulugbekgofurjonov001@gmail.com>
2026-04-02 17:44:48 +0500

    Project Start

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="bg-space-dark min-h-screen text-white flex items-center justify-center">
+      <h1 className="text-4xl font-bold">Space Helmet Landing</h1>
+      <p className="text-space-text-secondary mt-4">Tailwind ishlayapti! 🚀</p>
+    </div>
+  )
+}
+
+export default App
