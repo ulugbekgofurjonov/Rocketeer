c36eb0b7aa9f5076eb586352a6a3b28e3803a610
Ulug'bek G'ofurjonov <ulugbekgofurjonov001@gmail.com>
2026-04-02 18:55:41 +0500

    Data loaded.

---
+++ src/Components/data/index.js
@@ -0,0 +1,66 @@
+export const heroData = {
+    title: "The future is now",
+    image: "/images/bg1.png",
+    paragraph: "Rocketeer helmets is a visionary enterprise that is trying to improve your health by building modern and comfortable helmets for you.",
+    inputParagraph: "Enter your email to get the latest news",
+    inputPlaceholder: "Enter your email here",
+    buttonText: "Submit"
+};
+
+export const aboutData = {
+    title: "About Us",
+    secondTitle: "Our mission is to change the way you explore the space",
+    image: "/images/bg2.png",
+    paragraph: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Justo eget magna fermentum iaculis eu non diam phasellus. Sagittis orci a scelerisque purus semper eget. Auctor neque vitae tempus quam pellentesque nec nam aliquam. Non curabitur gravida arcu ac tortor. Penatibus et magnis dis parturient montes. Aliquam sem fringilla ut morbi tincidunt augue. Dictumst quisque sagittis purus sit amet volutpat consequat mauris nunc. Nunc sed id semper risus in hendrerit gravida rutrum. Senectus et netus et malesuada. Ornare arcu dui vivamus arcu felis. Purus semper eget duis at."
+};
+
+export const featuresData = [
+    {
+        image: "/images/bg3.png",
+        title: "Advanced communications system",
+        paragraph: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore, et dolore magna aliqua. Justo eget magna fermentum iaculis eu non diam phasellus.",
+        buttonText: "Buy now"
+    },
+    {
+        image: "/images/bg4.png",
+        title: "Improved ventilation system",
+        paragraph: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Justo eget magna fermentum iaculis eu non diam phasellus. ",
+        buttonText: "Buy now"
+    }
+];
+
+export const pricingData = [
+    {
+        plan: "Basic",
+        features: [
+            "diam maecenas ultricies mi",
+            "semper risus in hendrerit",
+            "ultricies integer quis",
+            "vitae auctor eu augue",
+            "faucibus interdum posuere lorem",
+        ],
+        price: "$230"
+    },
+    {
+        plan: "Premium",
+        features: [
+            "diam maecenas ultricies mi",
+            "semper risus in hendrerit",
+            "ultricies integer quis",
+            "vitae auctor eu augue",
+            "faucibus interdum posuere lorem",
+        ],
+        price: "$500"
+    },
+    {
+        plan: "Pro",
+        features: [
+            "diam maecenas ultricies mi",
+            "semper risus in hendrerit",
+            "ultricies integer quis",
+            "vitae auctor eu augue",
+            "faucibus interdum posuere lorem",
+        ],
+        price: "$860"
+    }
+]
